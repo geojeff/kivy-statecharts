bb82c3346fc84c7db73710d7aa5ea0cd8a0c6f4a
Jeff Pittman <geojeff@me.com>
2012-09-14 20:24:48 -0500

    Fixes for pep8.

--- examples/pong_no_kv/main.py
+++ examples/pong_no_kv/main.py
@@ -1,27 +1,27 @@
-import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.properties import (
+    NumericProperty,
+    ReferenceListProperty,
+    ObjectProperty,
+)
 from kivy.vector import Vector
-from kivy.factory import Factory
 from kivy.clock import Clock
 from kivy.graphics.vertex_instructions import Rectangle, Ellipse
 from kivy.uix.floatlayout import FloatLayout
 
-from random import randint, random
-
 from kivy_statecharts.system.state import State
-from kivy_statecharts.system.statechart import Statechart
 from kivy_statecharts.system.statechart import StatechartManager
+
 
 class Paddle(Widget):
     parent = ObjectProperty(None)
     score = NumericProperty(0)
     x = NumericProperty(0)
     y = NumericProperty(0)
-    
+
     def __init__(self):
-        super(Paddle, self).__init__(size=(25,200))
+        super(Paddle, self).__init__(size=(25, 200))
         with self.canvas:
             Rectangle(pos=self.pos, size=self.size)
 
@@ -38,10 +38,11 @@ class Paddle(Widget):
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
-            offset = (ball.center_y-self.center_y)/(self.height/2)
-            bounced = Vector(-1*vx, vy)
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
             vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset   
+            ball.velocity = vel.x, vel.y + offset
+
 
 class LeftPlayerPaddle(Paddle):
     def __init__(self, **kwargs):
@@ -53,23 +54,25 @@ class LeftPlayerPaddle(Paddle):
     def _center_y(self, *l):
         self.center_y = self.parent.center_y
 
+
 class RightPlayerPaddle(Paddle):
     def __init__(self, **kwargs):
         super(RightPlayerPaddle, self).__init__(**kwargs)
 
     def _x(self, *l):
-        self.x = self.parent.width-self.width
+        self.x = self.parent.width - self.width
 
     def _center_y(self, *l):
         self.center_y = self.parent.center_y
+
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
-    
+
     def __init__(self):
-        super(PongBall, self).__init__(size=(50,50))
+        super(PongBall, self).__init__(size=(50, 50))
         with self.canvas:
             Ellipse(pos=self.pos, size=self.size)
         self.bind(parent=self._center)
@@ -80,20 +83,21 @@ class PongBall(Widget):
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
+
 class PongGame(Widget):
     ball = ObjectProperty(None)
     playerLeft = ObjectProperty(None)
     playerRight = ObjectProperty(None)
 
     def __init__(self):
-        super(PongGame, self).__init__() 
+        super(PongGame, self).__init__()
 
         self.ball = PongBall()
         self.playerLeft = LeftPlayerPaddle()
         self.playerRight = RightPlayerPaddle()
-        
+
         with self.canvas:
-            Rectangle(pos=(self.center_x-5, 0), size=(10, self.height))
+            Rectangle(pos=(self.center_x - 5, 0), size=(10, self.height))
 
         self.add_widget(self.ball)
         self.add_widget(self.playerLeft)
@@ -102,17 +106,17 @@ class PongGame(Widget):
         self.playerLeft.initBindings()
         self.playerRight.initBindings()
 
-    def serve_ball(self, vel=(4,0)):
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
-        
+
     def update(self, *args):
         self.ball.move()
-        
+
         #bounce of paddles
         self.playerLeft.bounce_ball(self.ball)
         self.playerRight.bounce_ball(self.ball)
-        
+
         #bounce ball off bottom or top
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1
@@ -120,16 +124,17 @@ class PongGame(Widget):
         #went of to a side to score point?
         if self.ball.x < self.x:
             self.playerRight.score += 1
-            self.serve_ball(vel=(4,0))
+            self.serve_ball(vel=(4, 0))
         if self.ball.x > self.width:
             self.playerLeft.score += 1
-            self.serve_ball(vel=(-4,0))
-            
+            self.serve_ball(vel=(-4, 0))
+
     def on_touch_move(self, touch):
-        if touch.x < self.width/3:
+        if touch.x < self.width / 3:
             self.playerLeft.center_y = touch.y
-        if touch.x > self.width - self.width/3:
+        if touch.x > self.width - self.width / 3:
             self.playerRight.center_y = touch.y
+
 
 class MainView(FloatLayout):
     game = ObjectProperty(None)
@@ -138,6 +143,7 @@ class MainView(FloatLayout):
         super(MainView, self).__init__()
         self.game = game
         self.add_widget(self.game)
+
 
 ##############
 # Statechart
@@ -155,10 +161,11 @@ class AppStatechart(StatechartManager):
     #
     class RootState(State):
         def __init__(self, **kwargs):
-            super(AppStatechart.RootState, self).__init__(**kwargs)
-        
+            super(AppStatechart.
+                  RootState, self).__init__(**kwargs)
+
         initialSubstateKey = 'PongGame'
-        
+
         ###################
         # PongGame
         #
@@ -168,39 +175,49 @@ class AppStatechart(StatechartManager):
 
             def __init__(self, **kwargs):
                 kwargs['initialSubstateKey'] = 'InitializingGame'
-                super(AppStatechart.RootState.PongGame, self).__init__(**kwargs)
-        
+                super(AppStatechart.
+                      RootState.
+                      PongGame, self).__init__(**kwargs)
+
             def enterState(self, context=None):
                 print 'PongGame/enterState'
                 self.game = PongGame()
                 self.mainView = MainView(self.game)
-                        
+
             def exitState(self, context=None):
                 print 'PongGame/exitState'
 
             class InitializingGame(State):
                 def __init__(self, **kwargs):
                     kwargs['initialSubstateKey'] = 'ShowingGame'
-                    super(AppStatechart.RootState.PongGame.InitializingGame, self).__init__(**kwargs)
+                    super(AppStatechart.
+                          RootState.
+                          PongGame.
+                          InitializingGame, self).__init__(**kwargs)
 
                 def enterState(self, context=None):
                     print 'InitializingGame/enterState'
                     app = PongApp(self.parentState.mainView)
                     self.statechart.app = app
-    
+
                 def exitState(self, context=None):
                     print 'InitializingGame/exitState'
 
                 class ShowingGame(State):
                     def __init__(self, **kwargs):
-                        super(AppStatechart.RootState.PongGame.InitializingGame.ShowingGame, self).__init__(**kwargs)
-    
+                        super(AppStatechart.
+                              RootState.
+                              PongGame.
+                              InitializingGame.
+                              ShowingGame, self).__init__(**kwargs)
+
                     def enterState(self, context=None):
                         print 'ShowingGame/enterState'
                         self.statechart.app.run()
-        
+
                     def exitState(self, context=None):
                         print 'ShowingGame/exitState'
+
 
 class PongApp(App):
     mainView = ObjectProperty(None)
@@ -211,10 +228,9 @@ class PongApp(App):
 
     def build(self):
         self.mainView.game.serve_ball()
-        Clock.schedule_interval(self.mainView.game.update, 1.0/60.0)
+        Clock.schedule_interval(self.mainView.game.update, 1.0 / 60.0)
         return self.mainView.game
 
 if __name__ in ('__android__', '__main__'):
     statechart = AppStatechart()
     statechart.initStatechart()
-
