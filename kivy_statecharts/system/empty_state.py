9ea35f55ad2015d224dd53cb1fc7a84dc235556a
geojeff <geojeff@me.com>
2012-12-30 03:32:04 -0600

    Did more docstrings formatting.

--- kivy_statecharts/system/empty_state.py
+++ kivy_statecharts/system/empty_state.py
@@ -6,24 +6,19 @@
 
 from kivy_statecharts.system.state import State
 
-"""
-  The default name given to an empty state
-"""
+# The default name given to an empty state
 EMPTY_STATE_NAME = "__EMPTY_STATE__"
 
-"""
-  @class
-
-  Represents an empty state that gets assigned as a state's initial substate
-  if the state does not have an initial substate defined.
-
-  @extends State
-"""
 class EmptyState(State):
+    '''Represents an empty state that gets assigned as a state's initial
+       substate if the state does not have an initial substate defined.
+    '''
+
     def __init__(self, **kwargs):
         kwargs['name'] = EMPTY_STATE_NAME
         super(EmptyState, self).__init__(**kwargs)
 
     def enter_state(self, context=None):
-        msg = "No initial substate was defined for state {0}. Entering default empty state"
+        msg = ("No initial substate was defined for state {0}. "
+               "Entering default empty state")
         self.state_log_warning(msg.format(self.parent_state))
--- kivy_statecharts/system/history_state.py
+++ kivy_statecharts/system/history_state.py
@@ -7,69 +7,65 @@
 from kivy.properties import BooleanProperty, ObjectProperty, StringProperty
 from kivy_statecharts.system.state import State
 
-"""
-  @class
 
-  Represents a history state that can be assigned to a State object's
-  initial_substate property.
-
-  If a HistoryState object is assigned to a state's initial substate,
-  then after a state is entered the statechart will refer to the history
-  state object to determine the next course of action. If the state has
-  its history_state property assigned then that state will be entered,
-  otherwise the default state assigned to history state object will be entered.
-
-  [PORT] API is to use a state called InitialSubstate. See tests.
-
-  An example of how to use:
-
-    stateA = State()
-
-    stateA.initial_substate = HistoryState()
-    stateA.initial_substate.default_state = 'stateB'
-
-    stateA.stateB = State()
-    stateA.stateC = State()
-
-  @author Michael Cohen
-  @extends Object
-"""
 class HistoryState(State):
-    """
-      Used to indicate if the statechart should recurse the
-      history states after entering the this object's parent state
+    '''Represents a history state that can be assigned to a State object's
+       initial_substate property.
 
-      @property {Boolean}
-    """
+       If a HistoryState object is assigned to a state's initial substate, then
+       after a state is entered the statechart will refer to the history state
+       object to determine the next course of action. If the state has its
+       history_state property assigned then that state will be entered,
+       otherwise the default state assigned to history state object will be
+       entered.
+
+       [PORT] API is to use a state called InitialSubstate. See tests.
+
+       An example of how to use::
+
+           stateA = State()
+
+           stateA.initial_substate = HistoryState()
+           stateA.initial_substate.default_state = 'stateB'
+
+           stateA.stateB = State()
+           stateA.stateC = State()
+    '''
+
     is_recursive = BooleanProperty(False)
+    '''Used to indicate if the statechart should recurse the
+       history states after entering the this object's parent state
 
-    """
-      The default state to enter if the parent state does not
-      yet have its history_state property assigned to something
-      other than null.
+       :data:`is_recursive` is a :class:`~kivy.properties.BooleanProperty`,
+       default is False.
+    '''
 
-      The value assigned to this property must be the name of an
-      immediate substate that belongs to the parent state. The
-      statechart will manage the property upon initialization.
-
-      @property {String}
-    """
     default_state = StringProperty(None, allownone=True)
+    '''The default state to enter if the parent state does not
+       yet have its history_state property assigned.
+
+       The value assigned to this property must be the name of an immediate
+       substate that belongs to the parent state. The statechart will manage
+       the property upon initialization.
+
+       :data:`default` is a :class:`~kivy.properties.StringProperty`,
+       default is None.
+    '''
 
     def __init__(self, **kwargs):
         super(HistoryState, self).__init__(**kwargs)
 
-    """
-      Used by the statechart during a state transition process.
-
-      Returns a state to enter based on whether the parent state has
-      its history_state property assigned. If not then this object's
-      assigned default state is returned.
-
-      PORT: This was a simple computed property, so it could be observed.
-            Here, we make it a simple dynamic method.
-    """
     def state(self):
+        '''Used by the statechart during a state transition process.
+
+           Returns a state to enter based on whether the parent state has its
+           history_state property assigned. If not then this object's assigned
+           default state is returned.
+
+           PORT: This was a computed property, so it could be observed.
+                 Here, we make it a dynamic method.
+        '''
+
         default_state = self.get_state(self.default_state)
         history_state = self.parent_state.history_state
 
